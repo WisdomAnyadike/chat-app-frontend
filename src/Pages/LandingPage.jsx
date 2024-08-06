1177c1ebe155d0517f61454e60a8f8e669052400
WisdomAnyadike <anyadikewisdom@icloud.com>
2024-08-06 08:25:31 +0100

    new design

--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -2,7 +2,7 @@ import React, { useRef } from 'react'
 import Section1 from '../Components/ForLandingPage/Section1'
 import '/src/Components/ForLandingPage/section1.styles.scss'
 import Nav from '../Components/Navbar/Nav'
-import Section3 from '../Components/ForLandingPage/Section3'
+
 
 
 const LandingPage = () => {
@@ -12,8 +12,9 @@ const LandingPage = () => {
             <div ref={scrollRef} style={{ width: '100%', height: '100vh', overflowY: "scroll" }}>
                 <Nav scrollRef={scrollRef} />
                 <Section1 />
+               
 
-                <Section3 />
+              
             </div>
 
 
